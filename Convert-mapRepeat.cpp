ee1ad6f2ea5f58ee40e94ed73a86d0ae0eac5c17
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-11-29 09:38:39 -0500

    Add files via upload

--- Convert-mapRepeat.cpp
+++ Convert-mapRepeat.cpp
@@ -17,6 +17,7 @@ void choice() {
     cout << "//Enter c for strings with no quotation marks.\n";
     cout << "//Enter cy for what's needed to copy and paste to finish a simple program.\n";
     cout << "//Enter i for if statements e for else if for strings.\n";
+    cout << "//Enter mi for string if and else if with 2 conditions.\n";
     cout << "//Enter in for if and else if statements for numbers C-C++.\n";
     cout << "//Enter im for if and else if statements for numbers if malloc is used.\n";
     cout << "//Enter f for fgets.\n";
@@ -193,6 +194,96 @@ void elseIf() {
         std::cout << "//";
         getline(std::cin, value);
         std::cout << "\n" << f[0] << name << f[1] << f[2] << value << f[2] << f[3] << "\n\n";
+        if (repeat != "r") {
+            break;
+        }
+    }
+}
+
+void multiIf() {
+    std::string t[11] = {"if(strcmp(", ",", "} else if(strcmp(", "\"", ")==0", ")!=0", ")==0) {", ")!=0) {", " && ", " || ", "strcmp("};
+    std::string select;
+    std::string name;
+    std::string value;
+    std::string op;
+    std::string compare;
+    std::string sname;
+    std::string svalue;
+    std::string scompare;
+
+    while (true) {
+	std::cout << "//Enter i for if, e for else if.\n";
+        while (getline(std::cin,select)) {
+            if (select != "i" && select != "e") {
+	        std::cout << "//Enter i or e only.\n";
+            } else {
+                break;
+            }
+        }
+        if (select == "i") {
+            select = t[0];
+        } else if (select == "e") {
+            select = t[2];
+        }
+	std::cout << "//Enter a name of first m for main.\n";
+	std::cout << "//";
+        getline(std::cin,name);
+        if (name == "m") {
+            break;
+        }
+	std::cout << "//Enter a value.\n";
+	std::cout << "//";
+        getline(std::cin,value);
+	std::cout << "//Enter a comparison a for equal b for not equal.\n";
+	std::cout << "//";
+        while (getline(std::cin,compare)) {
+            if (compare != "a" && compare != "b") {
+	        std::cout << "//Enter a or b only.\n";
+            } else {
+                break;
+            }
+        }
+        if (compare == "a") {
+            compare = t[4];
+        } else if (compare == "b") {
+            compare = t[5];
+        }
+	std::cout << "//Enter a operator a for and o for or.\n";
+	std::cout << "//";
+        while (getline(std::cin,op)) {
+            if (op !=  "a" && op != "o") {
+	        std::cout << "//Enter a or o only.\n";
+            } else {
+                break;
+            }
+        }
+        if (op == "a") {
+            op = t[8];
+        } else if (op == "o") {
+            op = t[9];
+        }
+
+	std::cout << "//Enter the second name,\n";
+	std::cout << "//";
+        getline(std::cin,sname);
+	std::cout << "Enter the second value.\n";
+	std::cout << "//";
+        getline(std::cin,svalue);
+	std::cout << "Enter a second comparison a for equal b for not equal\n";
+	std::cout << "//";
+        while (getline(std::cin,scompare)) {
+            if (scompare != "a" && scompare != "b") {
+	        std::cout << "//Enter a or b only.\n";
+            } else {
+                break;
+            }
+        }
+	if (scompare == "a") {
+            scompare = t[6];
+        } else if (scompare == "b") {
+            scompare = t[7];
+        }
+        std::cout << "\n" << "\n" <<  select <<  name <<  t[1] << t[3] << value << t[3] << compare << op << t[10] << sname << t[1] << t[3] <<  svalue << t[3] <<  scompare << "\n\n";
         if (repeat != "r") {
             break;
         }
@@ -472,22 +563,7 @@ void callFunct() {
 void heap() {
     std::string t[9] = {"char", " *", ";", " = (char*)malloc", "(", ");", "* sizeof(char));", "free", " = NULL;"};
     std::string ch[6] = {"if(", " == NULL){", "    printf(\"", "\\n", "\");", "    exit(1);}"};
-    std::string i[15] = {"int",
-                         " *",
-                         ";",
-                         " = (int*)malloc",
-                         "(",
-                         ");",
-                         "* sizeof(int));",
-                         "free",
-                         " = NULL;",
-                         "double",
-                         "float",
-                         " = (double*)malloc",
-                         " = (float*)malloc",
-                         "* sizeof(double));",
-                         "* sizeof(float));"};
-
+    std::string i[15] = {"int"," *",";"," = (int*)malloc","(",");","* sizeof(int));","free"," = NULL;","double","float"," = (double*)malloc"," = (float*)malloc","* sizeof(double));","* sizeof(float));"};
     std::string name;
     std::string mem;
     std::string check;
@@ -1107,7 +1183,8 @@ int main() {
         {"s",           scanff},           
         {"sb",      scanfBasic},
         {"i",      ifStatement}, 
-        {"e",           elseIf}, 
+        {"e",           elseIf},
+        {"mi",         multiIf},	
         {"in",        ifNumber},  
         {"im",          ifHeap}, 
         {"v",         variable},     

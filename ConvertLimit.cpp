54bdd8983367d40a5abaef7d343f684de61b60e0
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2025-06-07 09:47:48 -0400

    Update ConvertLimit.cpp

--- ConvertLimit.cpp
+++ ConvertLimit.cpp
@@ -915,7 +915,7 @@ void heapNum() {
 }
 
 void checkMalloc() {
-    char t[6][18] = {"if(", " == NULL){", "    printf(\"", "\\n", "\");", "    exit(1);}"};
+    char t[8][18] = {"if(", " == NULL){", "    printf(\"", "\\n", "\");", "    exit(1);}","    exit(1);","}"};
     char name[SIZE];
     char em[SIZE];
     while (1) {
@@ -939,6 +939,13 @@ void checkMalloc() {
         printf("\n%s%s%s\n", t[0], name, t[1]);
         printf("%s%s%s%s\n", t[2], em, t[3], t[4]);
         printf("%s\n\n", t[5]);
+        printf("\n%s%s%s\n", t[0], name, t[1]);
+        printf("%s%s%s%s\n", t[2], em, t[3], t[4]);
+        printf("%s\n", t[6]);
+	printf("%s\n\n", t[7]);
+
+
+	    
         if (strcmp(repeat, "r") != 0) {
             break;
         }

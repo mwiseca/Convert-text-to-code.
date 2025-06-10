a3a1834f1f219329673436401e56a2a77d5d5451
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2025-06-10 08:43:52 -0400

    Update Convert.cpp

--- Convert.cpp
+++ Convert.cpp
@@ -22,6 +22,8 @@ void choice() {
     cout << "//Enter im for if and else if statements for numbers if malloc is used.\n";
     cout << "//Enter f for fgets.\n";
     cout << "//Enter fs for fgets with sizeof operator.\n";
+    cout << "//Enter fb for fgets to use with cb to clear input buffer without having ";
+    cout << "to press enter twice.\n";
     cout << "//Enter s for scanf for strings to allow for white spaces.\n";
     cout << "//Enter sb for scanf basic no white spaces.\n";
     cout << "//Enter si for scanf for integers and doubles.\n";
@@ -139,6 +141,65 @@ void fgetsSizeof() {
 	if (repeat != "r") {
 	    break;
 	}
+    }
+}
+
+void fgetsBuffer() {
+    std::string f[12] = {"fgets(", ",", "stdin);", "[strcspn(", "\"\\n", "\")]=0;", "while(", "      ", "stdin)) {","sizeof(","),stdin);","),stdin)) {"};
+    std::string v;
+    std::string me;
+    std::string function;
+    int m;
+
+    while (true) {
+        std::cout << "//Enter name of variable m for main.\n";
+	std::cout << "//";
+	getline(std::cin,v);
+	if (v  ==  "m") {
+	    break;
+	}
+	std::cout << "//Enter the memory to be allocated to the variable 5 minimum.\n";
+	std::cout << "//";
+        while(true) {
+	    try {	
+                getline(std::cin,me);	        
+	            m = std::stoi(me);
+	        if(m <=4) {
+	            std::cout << "//\nEnter a number 5 or more.\n\n";
+                } else {
+	            break;
+	        } 
+	    }catch(std::invalid_argument){
+	        std::cout << "\n//Enter a number only.\n\n";
+		}
+	 }
+	std::cout << "//enter the function name.\n";
+	std::cout << "//";
+	getline(std::cin,function);
+	std::cout << "\n" <<  f[0] << v << f[1] << me << f[1] << f[2] << "\n";
+	std::cout << v << f[3] << v << f[1] << f[4] << f[5] << "\n";
+	std::cout << "if(strlen(" << v << ")" <<  " >= "  << m-1 << ") {\n";
+	std::cout << "    " << function << "();\n";
+	std::cout << "}\n\n";	
+	std::cout << "\n" << f[6] << f[0] << v << f[1] << me << f[1] << f[8] << "\n";
+	std::cout << f[7] << v << f[3] << v << f[1] << f[4] << f[5] << "\n";
+	std::cout << "    if(strlen(" << v << ")" <<  " >= "  << m-1 << ") {\n";
+	std::cout << "        " << function << "();\n";
+	std::cout << "    }\n\n";
+
+	std::cout  << "\n" << f[0] << v << f[1] << f[9] << v << f[10] << "\n";
+	std::cout  <<  v << f[3] << v << f[1] << f[4] << f[5] << "\n";
+	std::cout << "if(strlen(" << v << ")" <<  " >= "  << m-1 << ") {\n";
+	std::cout << "    " << function << "();\n";
+	std::cout << "}\n\n";	
+	std::cout << "\n" << f[6] << f[0] << v << f[1] << f[9] << v << f[11] << "\n";	
+	std::cout << f[7] << v << f[3] << v << f[1] << f[4] << f[5] << "\n";
+	std::cout << "    if(strlen(" << v << ")" <<  " >= "  << m-1 << ") {\n";
+	std::cout << "        " << function << "();\n";
+	std::cout << "    }\n\n";	
+	if (repeat != "r") {
+	    break;
+        }	
     }
 }
 
@@ -1401,7 +1462,8 @@ int main() {
         {"a",           string},
         {"c",      stringEmpty}, 
         {"f",           fgetss},
-        {"fs",     fgetsSizeof},	
+        {"fs",     fgetsSizeof},
+	{"fb",     fgetsBuffer},
         {"s",           scanff},           
         {"sb",      scanfBasic},
 	{"cb",      clearBuffC},

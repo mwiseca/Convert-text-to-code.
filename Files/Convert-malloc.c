25823554e4688ae82f4b9496c52aaf84aa1d452a
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2025-10-30 08:33:22 -0400

    Update Convert-malloc.c

--- Files/Convert-malloc.c
+++ Files/Convert-malloc.c
@@ -1383,7 +1383,7 @@ void array() {
         }
         name[strcspn(name, "\n")] = 0;
         if(strlen(name) >= MAX) {
-            checkInput();
+            clean();
         }
         if (strcmp(name, "m") == 0) {
             free(name);

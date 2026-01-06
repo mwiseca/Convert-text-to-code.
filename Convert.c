4c3448d176a78067e5fba7028a48a0ec1c136cf0
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-01-06 09:44:10 -0500

    Implement stringToInt function for conversions
    Added stringToInt function to convert strings to integers and doubles.

--- Convert.c
+++ Convert.c
@@ -41,6 +41,7 @@ void choice() {
     printf("//Enter fb for fgets to use with cb to clear input buffer without having ");
     printf("to press enter twice.\n");
     printf("//Enter fcb to check fgets and clear input buffer after selecting cb.\n");
+    printf("//Enter fi to convert a string to a int or double to use with fgets.\n");
     printf("//Enter s for scanf for strings to allow for white spaces.\n");
     printf("//Enter sb for scanf basic no white spaces.\n");
     printf("//Enter si for scanf for integers and doubles.\n");
@@ -349,6 +350,136 @@ void checkFgets() {
         break;
         }
     }	    
+}
+
+void stringToInt() {
+    char  t [26] [25] = {"long int", ";","char *"," = strtol(","&",",10);","    ","printf(\"","\\n", "\");","} else if","(*"," == 0x20) {","printf(\"","\\n","\");","("," == ",") {"," != '\\0') {", "} else {",","," = strtod(",")"," &",");"};
+    char type [SIZE];
+    char string [SIZE];
+    char longInt [SIZE];
+    char ptrName [SIZE];
+    char rangePrintf [SIZE];
+    char spacesPrintf [SIZE]; 
+    char numberOnly [SIZE];
+    char textAfterNumber [SIZE];
+    while(1){
+        printf("//Enter type i for int d for double.\n");
+        while(fgets(type,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        type[strcspn(type,"\n")]=0;
+        if(strlen(type) >= MAX) {
+            clear();
+        }
+        if(strcmp(type,"i") !=0 && strcmp(type, "d")!=0){
+            printf("\n//Enter i or d only.\n\n");
+            continue;
+        }
+        printf("//Enter the string name to be converted m for main.\n");
+        while(fgets(string,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        string[strcspn(string,"\n")]=0;
+        if(strlen(string) >= MAX) {
+            clear();
+        }
+        if(strcmp(string,"m")==0) {
+            break;
+        }
+        printf("//Enter the converted integer or double name.\n");
+        while(fgets(longInt,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        longInt[strcspn(longInt,"\n")]=0;
+        if(strlen(longInt) >= MAX) {
+            clear();
+        }
+        printf("//Enter the name of the pointer endptr or ptr will do.\n");
+        while(fgets(ptrName,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        ptrName[strcspn(ptrName,"\n")]=0;
+        if(strlen(ptrName) >= MAX) {
+            clear();
+        }
+        printf("//Enter a error message for out of range.\n");
+        while(fgets(rangePrintf,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        rangePrintf[strcspn(rangePrintf,"\n")]=0;
+        if(strlen(rangePrintf) >= MAX) {
+            clear();
+        }
+        printf("//Enter a error message for spaces between numbers.\n");
+        while(fgets(spacesPrintf,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        spacesPrintf[strcspn(spacesPrintf,"\n")]=0;
+        if(strlen(spacesPrintf) >= MAX) {
+            clear();
+        }
+        printf("//Enter a error message for enter a number only.\n");
+        while(fgets(numberOnly,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        numberOnly[strcspn(numberOnly,"\n")]=0;
+        if(strlen(numberOnly) >= MAX) {
+            clear();
+        }
+        printf("//Enter a error message for text after number.\n");
+        while(fgets(textAfterNumber,SIZE,stdin) == NULL) {
+            checkInput(); 
+        }
+        textAfterNumber[strcspn(textAfterNumber,"\n")]=0;
+        if(strlen(textAfterNumber) >= MAX) {
+            clear();
+        }
+               printf("\n#include <errno.h>          Place at top of file.\n\n");
+        if(strcmp(type,"i")==0) {
+           printf("long int %s;\n",longInt);
+           printf("char *%s;\n",ptrName);
+        } else if(strcmp(type,"d")==0) {
+           printf("double %s;\n",longInt); 
+           printf("char *%s;\n\n",ptrName);
+        } 
+        printf("\nerrno = 0;\n");
+        if(strcmp(type,"i")==0) {
+            printf("%s%s%s%s%s%s%s\n",longInt,t[3],string,t[21],t[4],ptrName,t[5]);
+        } else if(strcmp(type,"d")==0) {
+            printf("%s%s%s%s%s%s%s\n",longInt,t[22],string,t[21], t[24],ptrName,t[25]);
+        }
+        printf("if(errno == ERANGE) {\n");
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],rangePrintf,t[8],t[8],t[9]);
+        printf("%s%s%s%s\n",t[10],t[11],ptrName,t[12]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],spacesPrintf,t[8],t[8],t[9]);
+        printf("%s%s%s%s%s%s\n",t[10],t[16],ptrName,t[17],string,t[18]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],numberOnly,t[8],t[8],t[9]);
+        printf("%s%s%s%s\n",t[10],t[11],ptrName,t[19]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],textAfterNumber,t[8],t[8],t[9]);
+        printf("%s\n\n",t[20]);
+        printf("\nerrno = 0;\n");
+        if(strcmp(type,"i")==0) {
+            printf("%s%s%s%s%s%s%s\n",longInt,t[3],string,t[21],t[4],ptrName,t[5]);
+        } else if(strcmp(type,"d")==0) {
+            printf("%s%s%s%s%s%s%s\n",longInt,t[22],string,t[21], t[24],ptrName,t[25]);
+        }
+        printf("if(errno == ERANGE) {\n");
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],rangePrintf,t[8],t[8],t[9]);
+        printf("%scontinue;\n",t[6]);
+        printf("%s%s%s%s\n",t[10],t[11],ptrName,t[12]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],spacesPrintf,t[8],t[8],t[9]);
+        printf("%scontinue;\n",t[6]);
+        printf("%s%s%s%s%s%s\n",t[10],t[16],ptrName,t[17],string,t[18]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],numberOnly,t[8],t[8],t[9]);
+        printf("%scontinue;\n",t[6]);
+        printf("%s%s%s%s\n",t[10],t[11],ptrName,t[19]);
+        printf("%s%s%s%s%s%s%s\n",t[6],t[7],t[8],textAfterNumber,t[8],t[8],t[9]);
+        printf("%scontinue;\n",t[6]);
+        printf("}\n\n");
+        if(strcmp(repeat,"r")!=0) {
+            break;
+        }       
+    } 
 }
 
 void scanff() {
@@ -2246,7 +2377,9 @@ int main() {
         } else if(strcmp(sw, "fb")==0) {
             fgetsBuffer(); 
         } else if(strcmp(sw, "fcb")==0) {
-            checkFgets();  
+            checkFgets();
+        } else if(strcmp(sw, "fi")==0) {
+            stringToInt();
         } else if(strcmp(sw, "s")==0) {
             scanff();
         } else if(strcmp(sw, "sb")==0) {

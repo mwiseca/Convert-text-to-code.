46e3497d5f4ac586a5e6b9b906de3da7a5857825
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-04-16 09:23:41 -0400

    Update copyright year to 2023-2026

--- ConvertC.cpp
+++ ConvertC.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023-2025 Mitchell E Wise
+// Copyright 2023-2026 Mitchell E Wise
 // SPDX-License-Identifier: Apache-20
 
 #include <functional>
@@ -2953,7 +2953,7 @@ int main() {
     char sw[100];
 
     printf("\n\n");
-    printf("          copyright 2025 Mitchell E Wise\n");
+    printf("          copyright 2023-2026 Mitchell E Wise\n");
     printf("          SPDX-License-Identifier: Apache-20\n\n\n");
 
     printf("//Enter r to repeat choices enter to not.\n");
